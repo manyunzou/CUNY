3fe34963fc3d92c2d26775d4a9fc5ff0a227d955
Mandy Zou <mz962@mail.missouri.edu>
2017-09-15 12:50:25 -0400

    Add second line

--- web-timeline/timeline.js
+++ web-timeline/timeline.js
@@ -1,4 +1,37 @@
 $(function(){
+
+    //First timeline
+    $(".circle1").mouseover(function() {
+        $(".law-title-1").show();
+        $(".law-details-1").show();
+    });
+
+    $(".circle1").mouseout(function() {
+        $(".law-title-1").hide();
+        $(".law-details-1").hide();
+    });
+
+    $(".circle2").mouseover(function(){
+        $(".law-title-2").show();
+        $(".law-details-2").show();
+    });
+
+    $(".circle2").mouseout(function() {
+        $(".law-title-2").hide();
+        $(".law-details-2").hide();
+    });
+
+    $(".circle3").mouseover(function(){
+        $(".law-title-3").show();
+        $(".law-details-3").show();
+    });
+
+    $(".circle3").mouseout(function() {
+        $(".law-title-3").hide();
+        $(".law-details-3").hide();
+    });
+
+    //Second timeline
     $(".circle1").mouseover(function() {
         $(".law-title-1").show();
         $(".law-details-1").show();
