7b1af92cd80859f8dcae7624208d630517b3da88
Mandy Zou <mz962@mail.missouri.edu>
2017-09-17 16:45:46 -0400

    add 1st expand area

--- web-timeline/timeline.js
+++ web-timeline/timeline.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    //First timeline
+    //1st timeline
     $(".circle1").mouseover(function() {
         $(".law-title-1").show();
         $(".law-details-1").show();
@@ -31,7 +31,14 @@ $(function(){
         $(".law-details-3").hide();
     });
 
-    //Second timeline
+    //1st timeline details expand
+    $(".bill01").click(function(){
+        $("#billdetail01").slideToggle(100, function(){
+            $(this).is(":visible")?"Collapse" : "Expand";
+        });
+    })
+
+    //2nd timeline
     $(".circle2_1").mouseover(function() {
         $(".law-title-2_1").show();
         $(".law-details-2_1").show();
@@ -60,5 +67,36 @@ $(function(){
     $(".circle2_3").mouseout(function() {
         $(".law-title-2_3").hide();
         $(".law-details-2_3").hide();
+    });
+
+    //3rd timeline
+    $(".circle3_1").mouseover(function() {
+        $(".law-title-3_1").show();
+        $(".law-details-3_1").show();
+    });
+
+    $(".circle3_1").mouseout(function() {
+        $(".law-title-3_1").hide();
+        $(".law-details-3_1").hide();
+    });
+
+    $(".circle3_2").mouseover(function(){
+        $(".law-title-3_2").show();
+        $(".law-details-3_2").show();
+    });
+
+    $(".circle3_2").mouseout(function() {
+        $(".law-title-3_2").hide();
+        $(".law-details-3_2").hide();
+    });
+
+    $(".circle3_3").mouseover(function(){
+        $(".law-title-3_3").show();
+        $(".law-details-3_3").show();
+    });
+
+    $(".circle3_3").mouseout(function() {
+        $(".law-title-3_3").hide();
+        $(".law-details-3_3").hide();
     });
 });
