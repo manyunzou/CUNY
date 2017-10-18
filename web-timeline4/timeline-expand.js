7332954bac875e6c17b567f6e257c9d9969932a5
Mandy Zou <mz962@mail.missouri.edu>
2017-10-18 13:52:41 -0400

    Change display attribute

--- web-timeline4/timeline-expand.js
+++ web-timeline4/timeline-expand.js
@@ -4,13 +4,13 @@ $(function(){
         var bp = document.getElementById("timeline-bp");
         var plusImg = document.getElementById("timeline-bp-plusSign");
 
-        if (bp.style.visibility === "hidden"){
-            $("#timeline-bp").css("visibility","visible");
+        if (bp.style.display === "none"){
+            $("#timeline-bp").css("display","block");
             $("#timeline-bp").css("height","5200px");
             plusImg.src = "img/minus-sign.png";
             // $("#timeline-plusSign").css("width","20px");
         } else {
-            $("#timeline-bp").css("visibility","hidden");
+            $("#timeline-bp").css("display","none");
             $("#timeline-bp").css("height","0");
             plusImg.src = "img/plus-sign.png";
         }
@@ -20,12 +20,12 @@ $(function(){
         var GTZ = document.getElementById("timeline-GTZ");
         var plusImg = document.getElementById("timeline-GTZ-plusSign");
 
-        if (GTZ.style.visibility === "hidden"){
-            $("#timeline-GTZ").css("visibility","visible");
+        if (GTZ.style.display === "none"){
+            $("#timeline-GTZ").css("display","block");
             $("#timeline-GTZ").css("height","600px");
             plusImg.src = "img/minus-sign.png";
         } else {
-            $("#timeline-GTZ").css("visibility","hidden");
+            $("#timeline-GTZ").css("display","none");
             $("#timeline-GTZ").css("height","0");
             plusImg.src = "img/plus-sign.png";
         }
