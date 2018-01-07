ca786618555546f807a03086386e042a15cee2f6
Mandy Zou <mz962@mail.missouri.edu>
2018-01-06 23:05:47 -0500

    Switch names

--- web-timeline6/timeline-expand.js
+++ web-timeline6/timeline-expand.js
@@ -1,11 +1,11 @@
 $(function(){
 
     $("#timeline-bp-colla").click(function(){
-        var bp = document.getElementById("timeline-0110");
+        var bp = document.getElementById("timeline-0104");
         var plusImg = document.getElementById("timeline-bp-plusSign");
 
         if (bp.style.display === "none"){
-            $("#timeline-0110").css("display","inline");
+            $("#timeline-0104").css("display","inline");
             $("#timeline-legend").css("display","block");
             plusImg.src = "img/minus-sign.png";
         } 
@@ -34,19 +34,19 @@ $(function(){
     //     $("#timeline-show0110 p").css("color","#5B5B5B");
     // });
 
-    $("#timeline-show0110").click(function(){
-        var part1 = document.getElementById("timeline-0110");
-        var part1open = document.getElementById("timeline-show0110");
+    $("#timeline-show0104").click(function(){
+        var part1 = document.getElementById("timeline-0104");
+        var part1open = document.getElementById("timeline-show0104");
 
         if (part1.style.display === "none"){
-            $("#timeline-0110").css("display","inline");
-            $("#timeline-show0110").css("display","none");
-            $("#timeline-hide0110").css("display","block");
+            $("#timeline-0104").css("display","inline");
+            $("#timeline-show0104").css("display","none");
+            $("#timeline-hide0104").css("display","block");
             $("#timeline-legend").css("display","block");
             $("#years-container-bp14").css("display","block");
         } else {
-            $("#timeline-0110").css("display","none");
-            $("#timeline-show0110").css("visibility","visible");
+            $("#timeline-0104").css("display","none");
+            $("#timeline-show0104").css("visibility","visible");
         }
     });
 
@@ -59,119 +59,160 @@ $(function(){
     //     $("#timeline-hide0110 p").css("color","#5B5B5B");
     // });
 
-    $("#timeline-hide0110").click(function(){
-        var part1 = document.getElementById("timeline-0110");
-        var part1open = document.getElementById("timeline-hide0110");
+    $("#timeline-hide0104").click(function(){
+        var part1 = document.getElementById("timeline-0104");
+        var part1open = document.getElementById("timeline-hide0104");
 
         if (part1.style.display === "inline"){
-            $("#timeline-0110").css("display","none");
-            $("#timeline-show0110").css("display","block");
-            $("#timeline-hide0110").css("display","none");
+            $("#timeline-0104").css("display","none");
+            $("#timeline-show0104").css("display","block");
+            $("#timeline-hide0104").css("display","none");
             $("#timeline-legend").css("display","none");
             $("#years-container-bp14").css("display","none");
         } else {
-            $("#timeline-0110").css("display","visible");
-            $("#timeline-show0110").css("visibility","hidden");
+            $("#timeline-0104").css("display","visible");
+            $("#timeline-show0104").css("visibility","hidden");
         }
     });
 
     // to show items from 05-10
-    // $("#timeline-show1120").mouseover(function(){
-    //     $("#timeline-show1120 p").css("color","#168DC5");
+    // $("#timeline-show0510").mouseover(function(){
+    //     $("#timeline-show0510 p").css("color","#168DC5");
     // });
 
-    // $("#timeline-show1120").mouseout(function(){
-    //     $("#timeline-show1120 p").css("color","#5B5B5B");
+    // $("#timeline-show0510").mouseout(function(){
+    //     $("#timeline-show0510 p").css("color","#5B5B5B");
     // });
 
-    $("#timeline-show1120").click(function(){
-        var part2 = document.getElementById("timeline-1120");
-        var part2open = document.getElementById("timeline-show1120");
+    $("#timeline-show0510").click(function(){
+        var part2 = document.getElementById("timeline-0510");
+        var part2open = document.getElementById("timeline-show0510");
 
         if (part2.style.display === "none"){
-            $("#timeline-1120").css("display","inline");
-            $("#timeline-show1120").css("display","none");
-            $("#timeline-hide1120").css("display","block");
+            $("#timeline-0510").css("display","inline");
+            $("#timeline-show0510").css("display","none");
+            $("#timeline-hide0510").css("display","block");
             $("#timeline-legend").css("display","block");
             $("#years-container-bp510").css("display","block");
         } else {
-            $("#timeline-1120").css("display","none");
-            $("#timeline-show1120").css("visibility","visible");
+            $("#timeline-0510").css("display","none");
+            $("#timeline-show0510").css("visibility","visible");
         }
     });
 
     // to hide items from 11-20
-    // $("#timeline-hide1120").mouseover(function(){
-    //     $("#timeline-hide1120 p").css("color","#168DC5");
+    // $("#timeline-hide0510").mouseover(function(){
+    //     $("#timeline-hide0510 p").css("color","#168DC5");
     // });
 
-    // $("#timeline-hide1120").mouseout(function(){
-    //     $("#timeline-hide1120 p").css("color","#5B5B5B");
+    // $("#timeline-hide0510").mouseout(function(){
+    //     $("#timeline-hide0510 p").css("color","#5B5B5B");
     // });
 
-    $("#timeline-hide1120").click(function(){
-        var part1 = document.getElementById("timeline-1120");
-        var part1open = document.getElementById("timeline-hide1120");
+    $("#timeline-hide0510").click(function(){
+        var part1 = document.getElementById("timeline-0510");
+        var part1open = document.getElementById("timeline-hide0510");
 
         if (part1.style.display === "inline"){
-            $("#timeline-1120").css("display","none");
-            $("#timeline-show1120").css("display","block");
-            $("#timeline-hide1120").css("display","none");
+            $("#timeline-0510").css("display","none");
+            $("#timeline-show0510").css("display","block");
+            $("#timeline-hide0510").css("display","none");
             $("#timeline-legend").css("display","none");
             $("#years-container-bp510").css("display","none");
         } else {
-            $("#timeline-1120").css("display","visible");
-            $("#timeline-show1120").css("visibility","hidden");
+            $("#timeline-0510").css("display","visible");
+            $("#timeline-show0510").css("visibility","hidden");
         }
     });
 
     // to show items from 11-14
-    // $("#timeline-show2130").mouseover(function(){
-    //     $("#timeline-show2130 p").css("color","#168DC5");
+    // $("#timeline-show1114").mouseover(function(){
+    //     $("#timeline-show1114 p").css("color","#168DC5");
     // });
 
-    // $("#timeline-show2130").mouseout(function(){
-    //     $("#timeline-show2130 p").css("color","#5B5B5B");
+    // $("#timeline-show1114").mouseout(function(){
+    //     $("#timeline-show1114 p").css("color","#5B5B5B");
     // });
 
-    $("#timeline-show2130").click(function(){
-        var part2 = document.getElementById("timeline-2130");
-        var part2open = document.getElementById("timeline-show2130");
+    $("#timeline-show1114").click(function(){
+        var part2 = document.getElementById("timeline-1114");
+        var part2open = document.getElementById("timeline-show1114");
 
         if (part2.style.display === "none"){
-            $("#timeline-2130").css("display","inline");
-            $("#timeline-show2130").css("display","none");
-            $("#timeline-hide2130").css("display","block");
+            $("#timeline-1114").css("display","inline");
+            $("#timeline-show1114").css("display","none");
+            $("#timeline-hide1114").css("display","block");
             $("#timeline-legend").css("display","block");
             $("#years-container-bp1114").css("display","block");
         } else {
-            $("#timeline-2130").css("display","none");
-            $("#timeline-show2130").css("visibility","visible");
+            $("#timeline-1114").css("display","none");
+            $("#timeline-show1114").css("visibility","visible");
         }
     });
 
     // to hide items from 21-30
-    // $("#timeline-hide2130").mouseover(function(){
-    //     $("#timeline-hide2130 p").css("color","#168DC5");
+    // $("#timeline-hide1114").mouseover(function(){
+    //     $("#timeline-hide1114 p").css("color","#168DC5");
     // });
 
-    // $("#timeline-hide2130").mouseout(function(){
-    //     $("#timeline-hide2130 p").css("color","#5B5B5B");
+    // $("#timeline-hide1114").mouseout(function(){
+    //     $("#timeline-hide1114 p").css("color","#5B5B5B");
     // });
 
-    $("#timeline-hide2130").click(function(){
-        var part1 = document.getElementById("timeline-2130");
-        var part1open = document.getElementById("timeline-hide2130");
+    $("#timeline-hide1114").click(function(){
+        var part1 = document.getElementById("timeline-1114");
+        var part1open = document.getElementById("timeline-hide1114");
 
         if (part1.style.display === "inline"){
-            $("#timeline-2130").css("display","none");
-            $("#timeline-show2130").css("display","block");
-            $("#timeline-hide2130").css("display","none");
+            $("#timeline-1114").css("display","none");
+            $("#timeline-show1114").css("display","block");
+            $("#timeline-hide1114").css("display","none");
             $("#timeline-legend").css("display","none");
             $("#years-container-bp1114").css("display","none");
         } else {
-            $("#timeline-2130").css("display","visible");
-            $("#timeline-show2130").css("visibility","hidden");
+            $("#timeline-1114").css("display","visible");
+            $("#timeline-show1114").css("visibility","hidden");
+        }
+    });
+
+    $("#timeline-show1530").click(function(){
+        var part2 = document.getElementById("timeline-1530");
+        var part2open = document.getElementById("timeline-show1530");
+
+        if (part2.style.display === "none"){
+            $("#timeline-1530").css("display","inline");
+            $("#timeline-show1530").css("display","none");
+            $("#timeline-hide1530").css("display","block");
+            $("#timeline-legend").css("display","block");
+            $("#years-container-bp1530").css("display","block");
+        } else {
+            $("#timeline-1530").css("display","none");
+            $("#timeline-show1530").css("visibility","visible");
+        }
+    });
+
+    // to hide items from 21-30
+    // $("#timeline-hide1114").mouseover(function(){
+    //     $("#timeline-hide1114 p").css("color","#168DC5");
+    // });
+
+    // $("#timeline-hide1114").mouseout(function(){
+    //     $("#timeline-hide1114 p").css("color","#5B5B5B");
+    // });
+
+    $("#timeline-hide1530").click(function(){
+        var part1 = document.getElementById("timeline-1530");
+        var part1open = document.getElementById("timeline-hide1530");
+
+        if (part1.style.display === "inline"){
+            $("#timeline-1530").css("display","none");
+            $("#timeline-show1530").css("display","block");
+            $("#timeline-hide1530").css("display","none");
+            $("#timeline-legend").css("display","none");
+            $("#years-container-bp1530").css("display","none");
+        } else {
+            $("#timeline-1530").css("display","visible");
+            $("#timeline-show1530").css("visibility","hidden");
         }
     });
 
